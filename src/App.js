209c7fe6b0069707edd95c21f9793212923ef493
Myron <dubtwee@gmail.com>
2020-11-14 01:25:32 -0600

    adding router setup for application with home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import Garden from './components/Garden/Garden';
 
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Welcome to your garden!</h1>
-    </header>
-    <Garden />
-  </div>
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to your garden!</h1>
+      </header>
+      <Route exact path="/" component={Garden} />
+    </div>
+  </Router>
 );
 
 export default App;
