680192d7aff4f567bec2c60b2173ceb863b5f8ed
Myron <dubtwee@gmail.com>
2020-11-14 00:24:22 -0600

    getting plants on page load

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const mapStateToProps = reduxState => ({
-    reduxState,
-});
-
 class PlantList extends Component {
-    componentDidMount() {
-        // use component did mount to dispatch an action to request the plantList from the API
-    }
+  componentDidMount() {
+    // use component did mount to dispatch an action to request the plantList from the API
+    this.props.dispatch({
+      type: 'GET_PLANTS',
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>This is the plant list</h3>
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h3>This is the plant list</h3>
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (reduxState) => ({
+  reduxState,
+});
 
 export default connect(mapStateToProps)(PlantList);
