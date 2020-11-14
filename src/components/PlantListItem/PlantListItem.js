b8f3042362d2df989ee7aa9fb8b54370f86cd99e
Myron <dubtwee@gmail.com>
2020-11-14 00:49:46 -0600

    adding a delete button and capturing the click

--- src/components/PlantListItem/PlantListItem.js
+++ src/components/PlantListItem/PlantListItem.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class PlantListItem extends Component {
+  handleClickDelete = (event) => {
+    console.log('plant:', this.props.plant);
+  };
+
   render() {
-    return <li>{this.props.plant.name}</li>;
+    return (
+      <li>
+        {this.props.plant.name}{' '}
+        <button onClick={this.handleClickDelete}>DELETE</button>
+      </li>
+    );
   }
 }
 
