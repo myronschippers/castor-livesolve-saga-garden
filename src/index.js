3ae0dc6f15922d39114a73234a86e7aec063567a
Myron <dubtwee@gmail.com>
2020-11-14 00:19:22 -0600

    adding saga setup and initial saga gen function

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
+// SAGA IMPORTS
+import createSagaMiddleware from 'redux-saga';
+import { takeLatest, put } from 'redux-saga/effects';
+import axios from 'axios';
+
 import App from './App';
 
 // this startingPlantArray should eventually be removed
@@ -15,6 +20,8 @@ const startingPlantArray = [
 
 const plantList = (state = startingPlantArray, action) => {
   switch (action.type) {
+    case 'SET_PLANT':
+      return action.payload;
     case 'ADD_PLANT':
       return [...state, action.payload];
     default:
@@ -22,10 +29,37 @@ const plantList = (state = startingPlantArray, action) => {
   }
 };
 
+//
+// SAGAS
+// ------------------------------
+
+function* rootSaga() {
+  yield takeLatest('GET_PLANTS', getPlants);
+}
+
+function* getPlants() {
+  try {
+    // make AJAX call to GET plants
+    const response = yield axios.get('/api/plant');
+
+    yield put({
+      type: 'SET_PLANT',
+      payload: response.data,
+    });
+  } catch (err) {
+    // surfacing an error message
+    console.log('Error getting plants:', err);
+  }
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   combineReducers({ plantList }),
-  applyMiddleware(logger)
+  applyMiddleware(logger, sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
